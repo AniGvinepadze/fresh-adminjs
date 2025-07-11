4fb674761428753cc62ddc155a9edf1faec91305
Ani <a.gvin3@gmail.com>
2025-07-11 22:30:44 +0400

    add dependency

--- dist/model/Post.model.js
+++
@@ -1,6 +0,0 @@
-import { model, Schema } from 'mongoose';
-export const PostSchema = new Schema({
-    title: { type: String, required: true },
-    imageUrl: { type: String },
-}, { timestamps: true });
-export const Post = model('Post', PostSchema);
